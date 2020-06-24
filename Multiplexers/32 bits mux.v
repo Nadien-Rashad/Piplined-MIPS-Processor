f64afd409048ed5776e2a3248ec3366689c2ae00
Nadien-Rashad <42560282+Nadien-Rashad@users.noreply.github.com>
2020-06-24 17:43:09 +0200

    Update 32 bits mux.v

--- Multiplexers/32 bits mux.v
+++ Multiplexers/32 bits mux.v
@@ -7,3 +7,14 @@ begin
 assign z = (sel==0)? x : (sel==1)? y : 32'bx;
 end
 endmodule
+
+
+module mux_32bit_with4(out,in1,sel); // mux32bit  implementation
+input [31:0] in1;
+input sel;
+output reg[31:0] out;
+always@(in1,sel)
+begin
+ out = (sel==1)? in1 : (sel==0)? 32'd1 : 32'bx;
+end
+endmodule
