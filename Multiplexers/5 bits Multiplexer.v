05d5b2e0e4d158ec87204fb1167b4466bb14c290
Nadien-Rashad <42560282+Nadien-Rashad@users.noreply.github.com>
2020-06-24 17:35:23 +0200

    Create 5 bits Multiplexer.v

---
+++ Multiplexers/5 bits Multiplexer.v
@@ -0,0 +1,10 @@
+module mux_5bit_with0(out,in1,sel); // mux32bit  implementation
+input [4:0] in1;
+input sel;
+output reg[4:0] out;
+always@(in1,sel)
+begin
+assign out = (sel==0)? in1 : (sel==1)? 5'b0 : 5'bx;
+end
+endmodule
+
