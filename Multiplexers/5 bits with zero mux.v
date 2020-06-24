247b88c51402950fe19940abedee7f2b0d00d0a7
Nadien-Rashad <42560282+Nadien-Rashad@users.noreply.github.com>
2020-06-24 17:37:54 +0200

    Create 5 bits with zero mux.v

---
+++ Multiplexers/5 bits with zero mux.v
@@ -0,0 +1,10 @@
+module mux_5bit(out,in1,in2,sel); // mux32bit  implementation
+input [4:0] in1,in2;
+input sel;
+output reg[4:0] out;
+always@(in1,in2,sel)
+begin
+assign out = (sel==0)? in1 : (sel==1)? in2 : 5'bx;
+end
+endmodule
+
